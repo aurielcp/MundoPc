bcfee41213af4c6f0bc0823939e3b4c393d26c6a
aurielcp <auriel.castroperez@gmail.com>
2022-11-29 19:39:56 -0600

    Creacion clase tipo Orden

---
+++ Orden.py
@@ -0,0 +1,20 @@
+class Orden:
+    contadorOrdenes = 0
+
+    def __init__(self, computadoras):
+        Orden.contadorOrdenes += 1
+        self._idOrden = Orden.contadorOrdenes
+        self._computadoras = computadoras
+
+    def agregarComputadora(self, computadora):
+        self._computadoras.append(computadora)
+
+    def __str__(self):
+        computadoras_str = ''
+        for computadora in self._computadoras:
+            computadoras_str += computadora.__str__()
+
+        return f'''
+            Orden: {self._idOrden}
+            Computadoras: {computadoras_str}
+        '''
